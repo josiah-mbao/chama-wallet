6b6d76f47d32772fc3a661bce2fd07c38c702a50
josiah-mbao <josiahmbaomc@gmail.com>
2025-11-01 15:48:28 +0300

    create db models

---
+++ backend/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from .database import Base
+
+class Member(Base):
+    __tablename__ = "members"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+
+    contributions = relationship("Contribution", back_populates="member")
+
+class Contribution(Base):
+    __tablename__ = "contributions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    amount = Column(Float)
+    member_id = Column(Integer, ForeignKey("members.id"))
+
+    member = relationship("Member", back_populates="contributions")
