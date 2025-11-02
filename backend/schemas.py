04eb8056d31c4d30037f0ad28d6d3437a286bec1
josiah-mbao <josiahmbaomc@gmail.com>
2025-11-02 14:09:42 +0300

    fix: replace old pydantic syntax for the Config class

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,29 +1,32 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from typing import List
 
 class ContributionBase(BaseModel):
     amount: float
 
+
 class ContributionCreate(ContributionBase):
     member_id: int
+
 
 class Contribution(ContributionBase):
     id: int
     member_id: int
 
-    class Config:
-        orm_mode = True
+    model_config = ConfigDict(from_attributes=True)
+
 
 class MemberBase(BaseModel):
     name: str
     email: str
 
+
 class MemberCreate(MemberBase):
     pass
+
 
 class Member(MemberBase):
     id: int
     contributions: List[Contribution] = []
 
-    class Config:
-        orm_mode = True
+    model_config = ConfigDict(from_attributes=True)
